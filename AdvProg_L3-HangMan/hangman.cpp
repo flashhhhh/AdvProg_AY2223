3b9785ac24fe642f61ef7713de756ac20b61b65d
flashhhhh <nguyenchithanhpro196@gmail.com>
2023-02-22 01:57:03 +0700

    commit

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -165,7 +165,7 @@ void processData(const char ch, const string& word,
             update incorrectChars: call updateEnteredChars() function
     ***/
 
-    if (isCharInWord(ch, word)) {
+    if (/*isCharInWord(ch, word)*/) {
         updateSecretWord(secretWord, ch, word);
         updateEnteredChars(ch, correctChars);
     }
